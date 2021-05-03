55d5da59754e8c5fa5fe198abe0dbb3b36feb12f
SanchezAlexis <54086948+SanchezAlexis@users.noreply.github.com>
2021-05-02 20:28:59 -0500

    Entrenamiento usando datos para predecir diabetes

---
+++ kerasDiabetes.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  1 12:28:08 2021
+
+@author: sanch
+"""
+
+
+########################################
+# model = Sequential()
+# model.add(Dense(12, input_dim=57, activation='relu'))
+# model.add(Dense(57, activation='relu'))
+# model.add(Dense(1, activation='sigmoid'))
+# # Compile model
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# # Fit the model
+# history = model.fit(X, y, validation_split=0.33, epochs=150, batch_size=10, verbose=0)
+# # list all data in history
+# print(history.history.keys())
+# # summarize history for accuracy
+# plt.plot(history.history['accuracy'])
+# plt.plot(history.history['val_accuracy'])
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+# # summarize history for loss
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+
+########################################3
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import sklearn
+
+# Import necessary modules
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+# Keras specific
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical 
+
+df = pd.read_csv('diabetes.csv') 
+print(df.shape)
+df.describe()
+
+target_column = ['Outcome'] 
+predictors = list(set(list(df.columns))-set(target_column))
+df[predictors] = df[predictors]/df[predictors].max()
+df.describe()
+
+X = df[predictors].values
+y = df[target_column].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=40)
+print(X_train.shape); print(X_test.shape)
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+count_classes = y_test.shape[1]
+print(count_classes)
+
+model = Sequential()
+model.add(Dense(500, activation='relu', input_dim=8))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+history=model.fit(X_train, y_train, epochs=200,validation_split=0.33)
+
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+pred_train= model.predict(X_train)
+scores = model.evaluate(X_train, y_train, verbose=0)
+print('Accuracy on training data: {}% \n Error on training data: {}'.format(scores[1], 1 - scores[1]))   
+ 
+pred_test= model.predict(X_test)
+scores2 = model.evaluate(X_test, y_test, verbose=0)
+print('Accuracy on test data: {}% \n Error on test data: {}'.format(scores2[1], 1 - scores2[1]))    
